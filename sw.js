adff3b6387cbee3a1abf2c88de7fd2384a9c66ef
ibrahimarn15-ctrl <ibrahim.arn15@gmail.com>
2026-03-28 00:35:11 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -76,3 +76,22 @@ function showAyat(lastIndex){
 
     setTimeout(sendNotification, 24*60*60*1000); // next day
 }
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+
+  const url = event.notification.data?.url;
+
+  event.waitUntil(
+    clients.matchAll({ type: "window", includeUncontrolled: true })
+    .then(windowClients => {
+      for (let client of windowClients) {
+        if (client.url === url && "focus" in client) {
+          return client.focus();
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow(url);
+      }
+    })
+  );
+});
